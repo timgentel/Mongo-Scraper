3de0b3f44925889233fa44ef6333d949b10ab3ef
Timothy Gentel <timgentel@Timothys-MacBook-Air.local>
2018-07-14 10:27:15 -0500

    push

---
+++ server.js
@@ -0,0 +1,41 @@
+//dependencies
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+//initialize Express app
+var express = require('express');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static(process.cwd() + '/public'));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// //connecting to MongoDB
+mongoose.connect('mongodb://heroku_p5gs5jb1:vkihv7grvdnioa27sbjnsvlrtf@ds159013.mlab.com:59013/heroku_p5gs5jb1');
+
+// mongoose.connect('mongodb://localhost/scraper_news');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose!')
+});
+
+var routes = require('./controller/controller.js');
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
+});
